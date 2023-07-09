7b91c0607e920ef2c94d96eb4871a2c08f378cbb
Kushnierova <Katerynakushnierova@gmail.com>
2023-07-09 17:44:39 +0200

    17:44

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,43 +1,47 @@
 import '../css/common.css';
 
 const bodyEl = document.querySelector('body');
-const startEl = document.querySelector('button[data-start]');
-const stopEl = document.querySelector('button[data-stop]');
-stopEl.disabled = true;
-let intervalID = null;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
-const randomBodyColorGenerator = {
-  DELAY: 1000,
+stopBtn.disabled = true;
+
+let timerId = null;
+
+function changeBgColorRandom() {
+  bodyEl.style.backgroundColor = `${randomBodyColorGen.getRandomHexColor()}`;
+}
+const randomBodyColorGen = {
 
   getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
   },
 
   interval() {
-    intervalID = setInterval(() => {
+    intervalId = setInterval(() => {
       changeBgColorRandom();
-    }, this.DELAY);
-    stopEl.disabled = false;
-  },
-
-  start() {
-    startEl.addEventListener('click', () => {
-      this.interval();
-      startEl.disabled = true;
-      stopEl.disabled = false;
-    });
-    stopEl.addEventListener('click', this.stop);
+    }, 1000);
+    stopBtn.disabled = false;
   },
 
   stop() {
-    clearInterval(intervalID);
-    stopEl.disabled = true;
-    startEl.disabled = false;
+    clearInterval(intervalId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
   },
+
+  start() {
+    startBtn.addEventListener('click', () => {
+      this.interval();
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+    });
+    stopBtn.addEventListener('click', this.stop);
+  },
+
+
 };
 
-function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${randomBodyColorGenerator.getRandomHexColor()}`;
-}
-
-randomBodyColorGenerator.start();
+randomBodyColorGen.start();
