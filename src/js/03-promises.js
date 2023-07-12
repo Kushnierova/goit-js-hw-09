d7ec65bbbb5a55e087a599d5be5c3e8df89132cc
Kushnierova <Katerynakushnierova@gmail.com>
2023-07-12 11:27:07 +0200

    11:27

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,7 @@
+
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
-// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay)
-// стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер
-// промісу (position), що створюється, і затримку, враховуючи першу затримку (delay),
-// введену користувачем, і крок (step).
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -16,11 +14,7 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-// Доповни код функції createPromise таким чином,
-// щоб вона повертала один проміс, який виконується або відхиляється через delay часу.
-// Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay
-// зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того,
-//  що потрібно зробити з промісом - виконати або відхилити.
+
 function handleSubmit(e) {
   e.preventDefault();
   const form = e.target;
@@ -28,9 +22,9 @@ function handleSubmit(e) {
   const stepInput = form.elements.step;
   const amountInput = form.elements.amount;
 
-  const firstDelay = Number(delayInput.value);
-  const step = Number(stepInput.value);
-  const amount = Number(amountInput.value);
+  const firstDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
 
   for (let i = 0; i < amount; i++) {
     const position = i + 1;
@@ -44,6 +38,5 @@ function handleSubmit(e) {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
-
   form.reset();
 }
